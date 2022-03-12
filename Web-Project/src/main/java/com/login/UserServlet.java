cb292561dd690be190facea44bc0797f6623b761
NINGWEII <nikaobolashvili8@gmail.com>
2022-03-13 02:19:11 +0400

    add user servlet

---
+++ Web-Project/src/main/java/com/login/UserServlet.java
@@ -0,0 +1,43 @@
+package com.login;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	
+	private UsersDao userDao = new UsersDao();
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		String profession = request.getParameter("profession");
+		
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setName(name);
+		user.setSurname(surname);
+		user.setProfession(profession);
+		
+		try {
+			userDao.registerUser(user);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("userpage.jsp");
+		rd.forward(request, response);
+		
+		
+	}
+
+}
